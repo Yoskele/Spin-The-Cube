1a547bff50f97d41c8852708547238d46bb8e76b
yossi <yossi.kayhko@gmail.com>
2023-04-29 16:02:19 +0300

    Added Gif

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a simple 3D cube that spins when a button is clicked. After it finishes spinning, the lid will open up and an item will be raised.
+
+![Alt text](../Animation.gif)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import {useState, useRef, useEffect} from 'react';
 import './App.css';
+import question from './images/question.png';
+import happySun from './images/happySun.png'
 
 function App() {
+  const cubeContainer = useRef(false)
+  const [spinCube, setSpinCube] = useState(false);
+  const topLid = useRef(null);
+  const sunImage = useRef(null);
+
+  useEffect(() => {
+    if(spinCube){
+      cubeContainer.current.style.animation = 'spin 0.5s linear infinite';
+      // After 3 sec open the lid.
+      setTimeout(() => {
+        cubeContainer.current.style.animation = 'none';
+        let drop = 50;
+        let lidInterval = setInterval(() => {
+          // Remove the Lid.
+          topLid.current.style.transform = `rotateX(90deg) translateZ(${drop}px)`
+          drop++
+          if(drop === 200){
+            clearInterval(lidInterval)
+            topLid.current.style.display = 'none';
+            // Display the Sun Image.
+            let x = 0;
+            let increase = setInterval(() => {
+              sunImage.current.style.opacity = `0.${x}`;
+              sunImage.current.style.bottom = `${x*x}px`;
+              x++;
+              if(x === 10){
+                clearInterval(increase)
+                sunImage.current.style.animation = 'spin 2s linear infinite';
+              }
+            },150)
+          }
+        },10)
+      },3000)
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cube-container">
+        <div className="cube" ref={cubeContainer}>
+          <div className='image-holder'>
+            <div className='image-wrapper'>
+              <img  ref={sunImage} src={happySun} alt="" />
+            </div>
+          </div>
+          <div className="cube-face cube-face-front">
+            <img className="questionMark" src={question} alt="" />
+          </div>
+          <div className="cube-face cube-face-back">
+            <img className="questionMark" src={question} alt="" />
+          </div>
+          <div className="cube-face cube-face-left">
+          <img className="questionMark" src={question} alt="" />
+          </div>
+          <div className="cube-face cube-face-right">
+          <img className="questionMark" src={question} alt="" />
+          </div>
+          <div className="cube-face cube-face-top" ref={topLid}></div>
+          <div className="cube-face cube-face-bottom" ></div>
+        </div>
+      </div>
+      { !spinCube ? (
+        <div>
+          <button
+            className='btn'
+            onClick={() => setSpinCube(!spinCube)}>
+              Spin
+          </button>
+        </div>
+        ) : (<p></p>)
+      }
     </div>
   );
 }
 
 export default App;
+
